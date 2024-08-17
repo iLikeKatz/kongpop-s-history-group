7362df953c44fb4faf07bda0270c21ac473e10bd
iLikekatz <151337253+iLikeKatz@users.noreply.github.com>
2024-08-17 23:00:15 +0700

    Add files via upload

--- pages/😎_kongpopswork.py
+++ pages/😎_kongpopswork.py
@@ -35,27 +35,27 @@ def kongpop():
     st.markdown("### 2.เพราะเหตุใดสยามจึงประกาศสงครามกับเยอรมนีและออสเตรีย-ฮังการี ")
 
     st.markdown("""
-    ### link 1 : <a href="https://en.wikipedia.org/wiki/Unrestricted_submarine_warfare" target="_blank">wiki (german submarine warfare)</a>
+    ### link 6 : <a href="https://en.wikipedia.org/wiki/Unrestricted_submarine_warfare" target="_blank">wiki (german submarine warfare)</a>
     """, unsafe_allow_html=True)
     st.text(" ")
     
     st.markdown("""
-    ### link 2 : <a href="https://www.acr.ac.th/acr/ACR_CAI/Chutikarn2/M31-56/M31Group9/M31group9/Situation.html" target="_blank">ARCCA</a>
+    ### link 7 : <a href="https://www.acr.ac.th/acr/ACR_CAI/Chutikarn2/M31-56/M31Group9/M31group9/Situation.html" target="_blank">ARCCA</a>
     """, unsafe_allow_html=True)
     st.text(" ")
 
     st.markdown("""
-    ### link 3 : <a href="https://www.gotoknow.org/posts/341547#google_vignette" target="_blank">Gotoknow</a>
+    ### link 8 : <a href="https://www.gotoknow.org/posts/341547#google_vignette" target="_blank">Gotoknow</a>
     """, unsafe_allow_html=True)
     st.text(" ")
 
     st.markdown("""
-    ### link 4 : <a href="https://www.bbc.com/thai/thailand-40670894" target="_blank">bbc news</a>
+    ### link 9 : <a href="https://www.bbc.com/thai/thailand-40670894" target="_blank">bbc news</a>
     """, unsafe_allow_html=True)
     st.text(" ")
 
     st.markdown("""
-    ### link 5 : <a href="https://www.tkpark.or.th/tha/tk_news_detail/261/%E0%B8%A3%E0%B8%B3%E0%B8%A5%E0%B8%B6%E0%B8%81%E0%B8%8A%E0%B8%B2%E0%B8%95%E0%B8%B4%E0%B8%AA%E0%B8%A2%E0%B8%B2%E0%B8%A1%E0%B8%81%E0%B8%B1%E0%B8%9A%E0%B8%AA%E0%B8%87%E0%B8%84%E0%B8%A3%E0%B8%B2%E0%B8%A1%E0%B9%82%E0%B8%A5%E0%B8%81%E0%B8%84%E0%B8%A3%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B8%97%E0%B8%B5%E0%B9%88-1" target="_blank">tk park</a>
+    ### link 10 : <a href="https://www.tkpark.or.th/tha/tk_news_detail/261/%E0%B8%A3%E0%B8%B3%E0%B8%A5%E0%B8%B6%E0%B8%81%E0%B8%8A%E0%B8%B2%E0%B8%95%E0%B8%B4%E0%B8%AA%E0%B8%A2%E0%B8%B2%E0%B8%A1%E0%B8%81%E0%B8%B1%E0%B8%9A%E0%B8%AA%E0%B8%87%E0%B8%84%E0%B8%A3%E0%B8%B2%E0%B8%A1%E0%B9%82%E0%B8%A5%E0%B8%81%E0%B8%84%E0%B8%A3%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B8%97%E0%B8%B5%E0%B9%88-1" target="_blank">tk park</a>
     """, unsafe_allow_html=True)
     st.text(" ")
     st.text(" ")
@@ -64,27 +64,27 @@ def kongpop():
     st.markdown("### 3.หลังจากสยามประกาศสงครามกับเยอรมนีและออสเตรีย-ฮังการี สยามทำอย่างไรต่อ")
 
     st.markdown("""
-    ### link 1 : <a href="https://en.wikipedia.org/wiki/Siamese_Expeditionary_Forces" target="_blank">wiki (siam arrival in europe)</a>
+    ### link 11 : <a href="https://en.wikipedia.org/wiki/Siamese_Expeditionary_Forces" target="_blank">wiki (siam arrival in europe)</a>
     """, unsafe_allow_html=True)
     st.text(" ")
     
     st.markdown("""
-    ### link 2 : <a href="https://encyclopedia.1914-1918-online.net/article/siam/" target="_blank">Ency</a>
+    ### link 12 : <a href="https://encyclopedia.1914-1918-online.net/article/siam/" target="_blank">Ency</a>
     """, unsafe_allow_html=True)
     st.text(" ")
 
     st.markdown("""
-    ### link 3 : <a href="https://www.thesiamsociety.org/wp-content/uploads/1982/03/JSS_070_0n_Hart_MilitaryParticipationOfSiamInWW1.pdf" target="_blank">the siam society</a>
+    ### link 13 : <a href="https://www.thesiamsociety.org/wp-content/uploads/1982/03/JSS_070_0n_Hart_MilitaryParticipationOfSiamInWW1.pdf" target="_blank">the siam society</a>
     """, unsafe_allow_html=True)
     st.text(" ")
 
     st.markdown("""
-    ### link 4 : <a href="https://anurakmag.com/art-and-culture/10/12/2023/volunteer-soldiers-and-the-role-of-siam-in-the-great-war-of-world-war-i/" target="_blank">anurak</a>
+    ### link 14 : <a href="https://anurakmag.com/art-and-culture/10/12/2023/volunteer-soldiers-and-the-role-of-siam-in-the-great-war-of-world-war-i/" target="_blank">anurak</a>
     """, unsafe_allow_html=True)
     st.text(" ")
 
     st.markdown("""
-    ### link 5 : <a href="https://www.tkpark.or.th/tha/tk_news_detail/261/%E0%B8%A3%E0%B8%B3%E0%B8%A5%E0%B8%B6%E0%B8%81%E0%B8%8A%E0%B8%B2%E0%B8%95%E0%B8%B4%E0%B8%AA%E0%B8%A2%E0%B8%B2%E0%B8%A1%E0%B8%81%E0%B8%B1%E0%B8%9A%E0%B8%AA%E0%B8%87%E0%B8%84%E0%B8%A3%E0%B8%B2%E0%B8%A1%E0%B9%82%E0%B8%A5%E0%B8%81%E0%B8%84%E0%B8%A3%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B8%97%E0%B8%B5%E0%B9%88-1" target="_blank">tk park</a>
+    ### link 15 : <a href="https://www.tkpark.or.th/tha/tk_news_detail/261/%E0%B8%A3%E0%B8%B3%E0%B8%A5%E0%B8%B6%E0%B8%81%E0%B8%8A%E0%B8%B2%E0%B8%95%E0%B8%B4%E0%B8%AA%E0%B8%A2%E0%B8%B2%E0%B8%A1%E0%B8%81%E0%B8%B1%E0%B8%9A%E0%B8%AA%E0%B8%87%E0%B8%84%E0%B8%A3%E0%B8%B2%E0%B8%A1%E0%B9%82%E0%B8%A5%E0%B8%81%E0%B8%84%E0%B8%A3%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B8%97%E0%B8%B5%E0%B9%88-1" target="_blank">tk park</a>
     """, unsafe_allow_html=True)
     st.text(" ")
     st.text(" ")
@@ -93,27 +93,27 @@ def kongpop():
     st.markdown("### 4.บริเวณการสู้รบหลักของสงครามโลกครั้งที่ 1 ที่ทหารอาสาไป คือที่ใด ")
 
     st.markdown("""
-    ### link 1 : <a href="https://www.blockdit.com/posts/601162c44537110cd40fa8b9" target="_blank">blockdit</a>
+    ### link 16 : <a href="https://www.blockdit.com/posts/601162c44537110cd40fa8b9" target="_blank">blockdit</a>
     """, unsafe_allow_html=True)
     st.text(" ")
     
     st.markdown("""
-    ### link 2 : <a href="https://www.bbc.com/thai/thailand-40670894" target="_blank">bbc news</a>
+    ### link 17 : <a href="https://www.bbc.com/thai/thailand-40670894" target="_blank">bbc news</a>
     """, unsafe_allow_html=True)
     st.text(" ")
 
     st.markdown("""
-    ### link 3 : <a href="https://medium.com/%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%A7%E0%B8%B1%E0%B8%95%E0%B8%B4%E0%B8%A8%E0%B8%B2%E0%B8%AA%E0%B8%95%E0%B8%A3%E0%B9%8C%E0%B9%82%E0%B8%A5%E0%B8%81/%E0%B8%AA%E0%B8%87%E0%B8%84%E0%B8%A3%E0%B8%B2%E0%B8%A1%E0%B9%82%E0%B8%A5%E0%B8%81%E0%B8%84%E0%B8%A3%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B8%97%E0%B8%B5%E0%B9%88-1-61a549c7c31e" target="_blank">medium</a>
+    ### link 18 : <a href="https://medium.com/%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%A7%E0%B8%B1%E0%B8%95%E0%B8%B4%E0%B8%A8%E0%B8%B2%E0%B8%AA%E0%B8%95%E0%B8%A3%E0%B9%8C%E0%B9%82%E0%B8%A5%E0%B8%81/%E0%B8%AA%E0%B8%87%E0%B8%84%E0%B8%A3%E0%B8%B2%E0%B8%A1%E0%B9%82%E0%B8%A5%E0%B8%81%E0%B8%84%E0%B8%A3%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B8%97%E0%B8%B5%E0%B9%88-1-61a549c7c31e" target="_blank">medium</a>
     """, unsafe_allow_html=True)
     st.text(" ")
 
     st.markdown("""
-    ### link 4 : <a href="https://www.posttoday.com/international-news/328462" target="_blank">posttoday</a>
+    ### link 19 : <a href="https://www.posttoday.com/international-news/328462" target="_blank">posttoday</a>
     """, unsafe_allow_html=True)
     st.text(" ")
 
     st.markdown("""
-    ### link 5 : <a href="https://thaiveterans.mod.go.th/6%E0%B8%AA%E0%B8%87%E0%B8%84%E0%B8%A3%E0%B8%B2%E0%B8%A1/%E0%B8%AA%E0%B8%87%E0%B8%84%E0%B8%A3%E0%B8%B2%E0%B8%A1%E0%B9%82%E0%B8%A5%E0%B8%81%E0%B8%84%E0%B8%A3%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B8%97%E0%B8%B5%E0%B9%88-1/" target="_blank">thaivet</a>
+    ### link 20 : <a href="https://thaiveterans.mod.go.th/6%E0%B8%AA%E0%B8%87%E0%B8%84%E0%B8%A3%E0%B8%B2%E0%B8%A1/%E0%B8%AA%E0%B8%87%E0%B8%84%E0%B8%A3%E0%B8%B2%E0%B8%A1%E0%B9%82%E0%B8%A5%E0%B8%81%E0%B8%84%E0%B8%A3%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B8%97%E0%B8%B5%E0%B9%88-1/" target="_blank">thaivet</a>
     """, unsafe_allow_html=True)
     st.text(" ")
     st.text(" ")
@@ -121,27 +121,27 @@ def kongpop():
     st.markdown("### 5.สนธิสัญญาเเวร์ซาย มีใจความหลักว่าอย่างไร")
 
     st.markdown("""
-    ### link 1 : <a href="https://th.wikipedia.org/wiki/%E0%B8%AA%E0%B8%99%E0%B8%98%E0%B8%B4%E0%B8%AA%E0%B8%B1%E0%B8%8D%E0%B8%8D%E0%B8%B2%E0%B9%81%E0%B8%A7%E0%B8%A3%E0%B9%8C%E0%B8%8B%E0%B8%B2%E0%B8%A2" target="_blank">wiki (เเวรซาย)</a>
+    ### link 21 : <a href="https://th.wikipedia.org/wiki/%E0%B8%AA%E0%B8%99%E0%B8%98%E0%B8%B4%E0%B8%AA%E0%B8%B1%E0%B8%8D%E0%B8%8D%E0%B8%B2%E0%B9%81%E0%B8%A7%E0%B8%A3%E0%B9%8C%E0%B8%8B%E0%B8%B2%E0%B8%A2" target="_blank">wiki (เเวรซาย)</a>
     """, unsafe_allow_html=True)
     st.text(" ")
     
     st.markdown("""
-    ### link 2 : <a href="https://ngthai.com/history/22347/versaillestreaty/" target="_blank">ngthai</a>
+    ### link 22 : <a href="https://ngthai.com/history/22347/versaillestreaty/" target="_blank">ngthai</a>
     """, unsafe_allow_html=True)
     st.text(" ")
 
     st.markdown("""
-    ### link 3 : <a href="https://forma-slova.com/th/articles/21354-treaty-of-versailles-summary-and-consequences" target="_blank">forma</a>
+    ### link 23 : <a href="https://forma-slova.com/th/articles/21354-treaty-of-versailles-summary-and-consequences" target="_blank">forma</a>
     """, unsafe_allow_html=True)
     st.text(" ")
 
     st.markdown("""
-    ### link 4 : <a href="https://hmong.in.th/wiki/Treaty_of_Versailles#google_vignette" target="_blank">hmong</a>
+    ### link 24 : <a href="https://hmong.in.th/wiki/Treaty_of_Versailles#google_vignette" target="_blank">hmong</a>
     """, unsafe_allow_html=True)
     st.text(" ")
 
     st.markdown("""
-    ### link 5 : <a href="https://worldcivil14.blogspot.com/2014/03/treaty-of-versailles.html" target="_blank">worldcivil</a>
+    ### link 25 : <a href="https://worldcivil14.blogspot.com/2014/03/treaty-of-versailles.html" target="_blank">worldcivil</a>
     """, unsafe_allow_html=True)
     st.text(" ")
     st.text(" ")
