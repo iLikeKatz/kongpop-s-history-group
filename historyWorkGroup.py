2aa18861a416acb36b43ef0b5c0c43925170f2ee
iLikekatz <151337253+iLikeKatz@users.noreply.github.com>
2024-08-17 23:01:11 +0700

    Add files via upload

--- historyWorkGroup.py
+++ historyWorkGroup.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Kongpop's history group(main)",
 )
+
 st.sidebar.success("Kongpop's history group")
 st.title("Kongpop's history group")
 st.markdown("### กดซ้ายบนเข้า tab")
